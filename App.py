a5a1f76fa8f1556d4b09ad5b6a3791c192d04490
AzmuthSzym <simondziadek@gmail.com>
2021-07-25 21:41:22 +0200

    More efficient Label generation

--- App.py
+++ App.py
@@ -20,16 +20,16 @@ def process() -> None:
     lat = lat_entry.get()
     lon = lon_entry.get()
     #my_APIkey = "YOUR API KEY"
+    my_APIkey = "07f9a8a2e678d62d713b90607cc1cfc3"
     my_Jsondata = getJson(my_APIkey, lat, lon)
     json_data = my_Jsondata.request_data()
     my_data = my_Jsondata.json_deserialize(json_data)
     air_data = my_data
-    values_label1 = Label(info_frame, text ="Concetration of CO[ug/m^3]: {}".format(air_data["list"][0]["components"]["co"])).grid(row = 0, column = 0)
-    values_label2 = Label(info_frame, text ="Concetration of NO[ug/m^3]: {}".format(air_data["list"][0]["components"]["no"])).grid(row = 1, column = 0)
-    values_label3 = Label(info_frame, text ="Concetration of NO2[ug/m^3]: {}".format(air_data["list"][0]["components"]["no2"])).grid(row = 2, column = 0)
-    values_label4 = Label(info_frame, text ="Concetration of O3[ug/m^3]: {}".format(air_data["list"][0]["components"]["o3"])).grid(row = 3, column = 0)
-    values_label5 = Label(info_frame, text ="Concetration of PM2_5[ug/m^3]: {}".format(air_data["list"][0]["components"]["pm2_5"])).grid(row = 4, column = 0)
-    values_label6 = Label(info_frame, text ="Concetration of PM10[ug/m^3]: {}".format(air_data["list"][0]["components"]["pm10"])).grid(row = 5, column = 0)
+    ctr = 0
+    for element in air_data["list"][0]["components"]:
+        values_label = Label(info_frame, text="Concetration of {}[ug/m^3]: {}".format(element.upper(), air_data["list"][0]["components"][element]))
+        values_label.grid(row=ctr, column=0, sticky=W)
+        ctr = ctr + 1
 
 
 if __name__ == "__main__":
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     
     root = tk.Tk()
     root.title("AirCheck")
-    root.geometry("260x250")
+    root.geometry("260x280")
     root.grid_rowconfigure(2, weight = 1)
     root.grid_columnconfigure(2, weight=1)
     reg = root.register(validate_input)
@@ -46,12 +46,12 @@ if __name__ == "__main__":
     top_frame.grid(padx=5, pady=5)
     
     lat_label = Label(top_frame, text = "Insert latitude here: ").grid(row = 0)
-    lat_entry = Entry(top_frame, width = 20, validate = "key", validatecommand = (reg, '%P'))
+    lat_entry = Entry(top_frame, width=20, validate="key", validatecommand=(reg, '%P'))
     lat_entry.grid(row = 0, column = 1)
     
     lon_label = Label(top_frame, text = "Insert longitude here: ").grid(row = 1)
-    lon_entry = Entry(top_frame, width = 20, validate = "key", validatecommand = (reg, '%P'))
-    lon_entry.grid(row = 1, column = 1)
+    lon_entry = Entry(top_frame, width=20, validate="key", validatecommand=(reg, '%P'))
+    lon_entry.grid(row=1, column=1)
     
     showinfo_btn = tk.Button(top_frame, text="App Info", command=popup_showinfo).grid(row = 2, column = 0, pady = 5)
     submit_btn = Button(top_frame, text = "Submit", command = process).grid(row = 2, column = 1, pady = 5)
